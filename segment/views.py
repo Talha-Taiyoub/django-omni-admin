f9eee9d1811a674744347e766b87300d87f64738
TalhaAnik56 <aniktalha@gmail.com>
2024-05-30 02:17:56 +0600

    created BranchViewSet

--- segment/views.py
+++ segment/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Branch, Destination, Slider
+from .serializers import BranchSerializer, SliderSerializer
+
+
+class BranchViewSet(ModelViewSet):
+    queryset = (
+        Branch.objects.all().select_related("destination").prefetch_related("sliders")
+    )
+    serializer_class = BranchSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    # def perform_create(self, serializer):
+    #     serializer.save()
