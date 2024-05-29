56a1e73e0f89dc709df0af3db06836b30e58c50b
TalhaAnik56 <aniktalha@gmail.com>
2024-05-29 22:24:59 +0600

    created segment app

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "segment",
     # customise app for general purpose
     "general_app",
 ]
