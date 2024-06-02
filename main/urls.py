9ebf9a8a7cf403b63e6e3fea75ea300d2931c2bf
TalhaAnik56 <aniktalha@gmail.com>
2024-06-03 04:49:52 +0600

    added password changed message

--- main/urls.py
+++ main/urls.py
@@ -18,10 +18,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_nested.routers import DefaultRouter
+
+from general_app.views import CustomUserViewSet
+
+router = DefaultRouter()
+router.register("users", CustomUserViewSet, basename="user")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("segments/", include("segment.urls")),
+    path("auth/", include(router.urls)),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    path("segments/", include("segment.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
