dab1fbb2722780ef77f4519e43a3d71379d86f93
TalhaAnik56 <aniktalha@gmail.com>
2024-06-04 08:05:35 +0600

    added a comment to custom exception handler

--- general_app/exception_handlers.py
+++ general_app/exception_handlers.py
@@ -1,12 +1,8 @@
-from rest_framework.exceptions import (
-    AuthenticationFailed,
-    NotAuthenticated,
-    ValidationError,
-)
 from rest_framework.views import exception_handler
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 
+# Created this so that The error message of invalid token will follow a format. Had to register this in the settings of REST_FRAMEWORK of settings.py file
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
