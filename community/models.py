68481a17272ee3673838781f60a6b125b989b2c7
TalhaAnik56 <aniktalha@gmail.com>
2024-06-05 22:29:37 +0600

    added Guest model

--- community/models.py
+++ community/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+# Create your models here
+
+ACTIVE = "active"
+INACTIVE = "inactive"
+STATUS_CHOICES = [(ACTIVE, "Active"), (INACTIVE, "Inactive")]
+
+
+class Guest(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    name = models.CharField(max_length=25, null=True, blank=True)
+    mobile = models.CharField(max_length=14, null=True, blank=True)
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, default=ACTIVE)
+    county = models.CharField(max_length=30, null=True, blank=True)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    postal_code = models.CharField(max_length=30, null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
