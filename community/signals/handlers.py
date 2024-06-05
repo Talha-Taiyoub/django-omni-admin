25f9d6102db48f8e02eb2c9daf30982bcf96573b
TalhaAnik56 <aniktalha@gmail.com>
2024-06-05 22:42:47 +0600

    implemented create guest for new user

--- community/signals/handlers.py
+++ community/signals/handlers.py
@@ -2,9 +2,11 @@ from django.dispatch import receiver
 from djoser.signals import user_activated
 
 from account.views import CustomUserViewSet
+from community.models import Guest
 
 
 @receiver(user_activated, sender=CustomUserViewSet)
 def create_guest_for_new_user(sender, **kwargs):
-    print("I'm receiving the Signal")
-    print(kwargs["user"].email)
+    user = kwargs["user"]
+    if not Guest.objects.filter(user=user).exists():
+        Guest.objects.create(user=user)
