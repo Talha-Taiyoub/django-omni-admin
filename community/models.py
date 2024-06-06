dddc03352af8b9fbbaaa884f352051459d28e7a7
TalhaAnik56 <aniktalha@gmail.com>
2024-06-07 02:59:54 +0600

    created staff model

--- community/models.py
+++ community/models.py
@@ -24,9 +24,36 @@ class Guest(models.Model):
     image = models.ImageField(
         upload_to="community/images", validators=[image_max_size], null=True, blank=True
     )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+ROLE_CHOICES = [
+    ("Operations", "Operations"),
+    ("Sales", "Sales"),
+    ("HR", "HR"),
+    ("Branch Manager", "Branch Manager"),
+]
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    name = models.CharField(max_length=25, null=True, blank=True)
+    mobile = models.CharField(max_length=14, null=True, blank=True)
+    role = models.CharField(max_length=16, choices=ROLE_CHOICES)
+    county = models.CharField(max_length=30, null=True, blank=True)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    postal_code = models.CharField(max_length=30, null=True, blank=True)
+    image = models.ImageField(
+        upload_to="community/images", validators=[image_max_size], null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
