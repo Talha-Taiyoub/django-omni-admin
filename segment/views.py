7900d43ffad5b8a84e8250e0716265067d841bb2
TalhaAnik56 <aniktalha@gmail.com>
2024-06-07 01:41:02 +0600

    created BranchViewSet

--- segment/views.py
+++ segment/views.py
@@ -1,11 +1,10 @@
 from rest_framework import status
-from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from general_app.format_response import format_response_data
 
-from .models import Branch, Destination
+from .models import Branch, BranchSlider, Destination
 from .serializers import BranchSerializer, DestinationSerializer
 
 
@@ -23,17 +22,36 @@ class DestinationViewSet(ModelViewSet):
         )
         return Response(custom_response, status=status.HTTP_200_OK)
 
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        custom_response = format_response_data(
+            message="Fetched the destination successfully.",
+            status_code=200,
+            data=response.data,
+        )
+        return Response(custom_response, status=status.HTTP_200_OK)
+
 
 class BranchViewSet(ModelViewSet):
     queryset = (
         Branch.objects.all().select_related("destination").prefetch_related("sliders")
     )
     serializer_class = BranchSerializer
-    parser_classes = [MultiPartParser, FormParser]
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        branch = serializer.save()
-        serializer = BranchSerializer(branch)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        custom_response = format_response_data(
+            message="Fetched all the branches successfully.",
+            status_code=200,
+            data=response.data,
+        )
+        return Response(custom_response, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        custom_response = format_response_data(
+            message="Fetched the branch successfully.",
+            status_code=200,
+            data=response.data,
+        )
+        return Response(custom_response, status=status.HTTP_200_OK)
