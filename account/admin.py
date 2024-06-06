95059e560d0408b417aa237bf29d703bb95500c6
TalhaAnik56 <aniktalha@gmail.com>
2024-06-07 03:50:41 +0600

    created User Admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "email",
+        "password",
+        "is_active",
+        "is_superuser",
+        "is_staff",
+        "last_login",
+        "date_joined",
+    ]
