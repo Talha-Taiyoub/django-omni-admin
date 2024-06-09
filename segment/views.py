1976cbc206fa966df0ae37072550370c79de51fd
TalhaAnik56 <aniktalha@gmail.com>
2024-06-10 03:21:20 +0600

    optimized BranchViewSet

--- segment/views.py
+++ segment/views.py
@@ -23,34 +23,19 @@ class DestinationViewSet(CustomResponseMixin, ModelViewSet):
     queryset = Destination.objects.all().prefetch_related("branches")
     serializer_class = DestinationSerializer
 
-    list_message = "All the destinations are fetched successfully"
-    retrieve_message = "The destination is fetched successfully"
+    list_message = "All the destinations are fetched successfully."
+    retrieve_message = "The destination is fetched successfully."
 
 
-class BranchViewSet(ModelViewSet):
+class BranchViewSet(CustomResponseMixin, ModelViewSet):
     http_method_names = ["get"]
     queryset = (
         Branch.objects.all().select_related("destination").prefetch_related("sliders")
     )
     serializer_class = BranchSerializer
 
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
-        custom_response = format_response_data(
-            message="Fetched all the branches successfully.",
-            status_code=200,
-            data=response.data,
-        )
-        return Response(custom_response, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, *args, **kwargs):
-        response = super().retrieve(request, *args, **kwargs)
-        custom_response = format_response_data(
-            message="Fetched the branch successfully.",
-            status_code=200,
-            data=response.data,
-        )
-        return Response(custom_response, status=status.HTTP_200_OK)
+    list_message = "Fetched all the branches successfully."
+    retrieve_message = "Fetched the branch successfully."
 
 
 # Created this view so that the frontend dev can get all the sliders at once
