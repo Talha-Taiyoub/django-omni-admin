93b4f4d518c0dcbe8521862d2953556e4d3a6c89
TalhaAnik56 <aniktalha@gmail.com>
2024-06-28 16:56:46 +0600

    created serializers and viewset for Restaurant

---
+++ refuel/serializers.py
@@ -0,0 +1,53 @@
+# To avoid circular dependencies, we will not use anything in segment.serializer from this module
+from rest_framework import serializers
+
+from segment.models import Branch
+
+from .models import Gallery, Restaurant, RestaurantCuisine
+
+
+class RestaurantCuisineSerializer(serializers.ModelSerializer):
+    cuisine = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = RestaurantCuisine
+        fields = ["id", "cuisine"]
+
+
+class GallerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gallery
+        fields = ["id", "image"]
+
+
+# I know I can inherit SimpleBranchSerializer from segments.serializer. But I want to keep it here.
+class VerySimpleBranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ["id", "name"]
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    branch = VerySimpleBranchSerializer(read_only=True)
+    cuisines = RestaurantCuisineSerializer(many=True, read_only=True)
+    gallery_set = GallerySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Restaurant
+        fields = [
+            "id",
+            "name",
+            "branch",
+            "status",
+            "overview",
+            "featured_image",
+            "gallery_set",
+            "cuisines",
+            "breakfast_opening",
+            "breakfast_closing",
+            "lunch_opening",
+            "lunch_closing",
+            "dinner_opening",
+            "dinner_closing",
+            "created_at",
+        ]
--- refuel/views.py
+++ refuel/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from general_app.format_response import CustomResponseMixin
+
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 
 # Create your views here.
+
+
+class RestaurantViewSet(ModelViewSet):
+    queryset = (
+        Restaurant.objects.all()
+        .select_related("branch")
+        .prefetch_related("cuisines", "gallery_set")
+    )
+    serializer_class = RestaurantSerializer
