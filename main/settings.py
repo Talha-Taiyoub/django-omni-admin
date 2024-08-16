c47172f9de0c04b31fbfe3793e83a5b7bb38ce0a
TalhaAnik56 <aniktalha@gmail.com>
2024-08-16 23:00:27 +0600

    made debug true

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "qg@s9od+31db&hrz2ftt7htee*a)eg8oq+y4(fihsb+7*4gjp6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
